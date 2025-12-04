92411650ea4ab2cdb5fba9a33a34bcc4781717cc
Fırat Alçın <firatalcin04@gmail.com>
2025-12-05 00:41:59 +0300

    Interceptor

---
+++ Arkitek/Arkitek.DataAccess/Interceptors/AuditDbContextInterceptor.cs
@@ -0,0 +1,46 @@
+﻿using Arkitek.Entity.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arkitek.DataAccess.Interceptors
+{
+    public class AuditDbContextInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in eventData.Context.ChangeTracker.Entries())
+            {
+                if(entry.Entity is not BaseEntity baseEntity)
+                {
+                    continue; 
+                }
+
+                if(entry.State is EntityState.Added)
+                {
+                    eventData.Context.Entry(baseEntity).Property(x => x.CreatedAt).CurrentValue = DateTime.UtcNow;
+                    eventData.Context.Entry(baseEntity).Property(x => x.UpdatedAt).IsModified = false;
+                }
+
+                if (entry.State is EntityState.Modified)
+                {
+                    eventData.Context.Entry(baseEntity).Property(x => x.CreatedAt).IsModified = false;
+                    eventData.Context.Entry(baseEntity).Property(x => x.UpdatedAt).CurrentValue = DateTime.UtcNow;
+                }
+
+                if (entry.State is EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    baseEntity.IsDeleted = true;
+                    eventData.Context.Entry(baseEntity).Property(x => x.CreatedAt).IsModified = false;
+                    eventData.Context.Entry(baseEntity).Property(x => x.UpdatedAt).CurrentValue = DateTime.UtcNow;
+                }
+            }
+
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
